1f0ca69421c53e7dab2588207ed6e3b0f1384891
Beslan <105642664+bratbes@users.noreply.github.com>
2024-07-16 20:27:52 +0300

    Add files via upload

--- task1/task1.py
+++ task1/task1.py
@@ -1,5 +1,7 @@
-n, m= int(input('n =')), int(input('m ='))
-j, f, i = 4, 1, 0 #Длина обхода, следующее число, инкремент
+import sys
+n = int(sys.argv[1])
+m = int(sys.argv[2])
+j, f, i = 5, 1, 0 #Длина обхода, следующее число, инкремент
 for i in range (j):
     print(f, end = '')
     f = 1 + (f + m - 2) % n

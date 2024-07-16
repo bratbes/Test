e788dd402e5f4ef019fdb93681f7816555f64b59
Beslan <105642664+bratbes@users.noreply.github.com>
2024-07-16 20:29:07 +0300

    Add files via upload

--- task4/task4.py
+++ task4/task4.py
@@ -1,4 +1,5 @@
-with open('file1.txt', 'r') as f:
+import sys
+with open(sys.argv[1], 'r') as f:
     num = list(map(int, f.readlines()))
 avg = sum(num)//len(num)
 count, i = 0, 0
@@ -10,4 +11,4 @@ while num.count(avg) != len(num):
     if num[i] == avg:
         i += 1
     count += 1
-    print ( count)
+print ( count)
